9f3bdec89031824b2dd992147226c3d99f41d98c
Gary J <github@accu.ca>
2022-03-24 20:48:54 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'App.css';
 
-function App() {
+export default function App() {
 
   return (
     <div className="App">
@@ -10,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;

32b47bf30d99cb15dcf76433d263aa42677d7305
gary <github@accu.ca>
2023-08-13 11:53:00 -0700

    Update

--- src/Quote.js
+++ src/Quote.js
@@ -1,14 +1,24 @@
 import useAxios from 'hooks/useAxios';
+import {useState} from 'react';
 
 const Quote = function(props) {
-  const {body, pending, error} = useAxios("https://api.office.rest");
+  const [show, setShow] = useState(false);
+
+  const {body, pending, error} = useAxios("https://api.office.rest/character");
   // const {body, pending, error} = useAxios("https://api.kanye.rest");
+
+  const text = body?.quote;
+  const character = body?.character;
 
   return (
     <li>
-      {!!body && body.quote}
       {pending && "Please Wait..."}
       {!!error && error}
+
+      {text}
+      {show && <div>{character}</div>}
+
+      {!show && text && <button onClick={() => setShow(true)}>who said that?</button>}
     </li>
   );
 
